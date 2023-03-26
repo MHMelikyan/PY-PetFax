0c5d31f930cee5a5407c6f7555d5a304b7cd8013
Melik Melikyan <mhmelikyan@gmail.com>
2023-03-26 10:38:10 -0400

    part 5 done

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -3,8 +3,7 @@ from flask import Flask
 # factory
 def create_app():
     app = Flask(__name__)
-    from . import pet
-    from . import fact
+    
     
     #index
     @app.route('/')
@@ -12,9 +11,11 @@ def create_app():
         return 'Hello,PetFax!'
 
     #register pet blueprint
+    from . import pet
     app.register_blueprint(pet.bp)
 
     #register fact plueprint
+    from . import fact
     app.register_blueprint(fact.bp)
 
     #return  the app

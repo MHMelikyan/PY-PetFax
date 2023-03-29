b1ed2a2014c05b7ebe6a8cdcce04871491ece127
Melik Melikyan <mhmelikyan@gmail.com>
2023-03-28 20:15:51 -0400

    part 7 update

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -9,13 +9,12 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Mebane521@localhost:5432/petfax'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False             
     
-    #DB config
     from . import models
     models.db.init_app(app)
     migrate= Migrate(app, models.db)
     #index
     @app.route('/')
-    def hello():
+    def index():
         return 'Hello,PetFax!'
 
     #register pet blueprint
